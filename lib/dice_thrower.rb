b67772776dc8c0b03e85b0b46863fcbceed637d5
maxwellbenton <maxwellbenton@gmail.com>
2018-10-26 10:52:40 -0400

    added solution

--- lib/dice_thrower.rb
+++ lib/dice_thrower.rb
@@ -1,1 +1,12 @@
 # Write your solution here
+def throw_dice
+  rand(6) + 1
+end
+
+def generate_set(number_of_throws)
+  array = Array.new
+  number_of_throws.times do
+    array << throw_dice
+  end
+  array
+end
